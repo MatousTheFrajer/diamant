d14c4a04dfe24fd5bec1236e74c697b6e054b428
Matouš Bien <matous.bien.020@pslib.cz>
2021-12-19 21:48:47 +0100

    Přidejte soubory projektu.

---
+++ diamant.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	int x = 9;
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < x; j++) {
+			printf(" ");
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int k = 2; k <= i; k++) {
+			printf("%d", k);
+		}
+		printf("\n");
+		x--;
+	}
+	x = 0;
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < x; j++) {
+			printf(" ");
+		}
+		for (int k = i; k >= 1; k--) {
+			printf("%d", k);
+		}
+		for (int y = 2; y <= i; y++) {
+			printf("%d", y);
+		}
+		printf("\n");
+		x++;
+	}
+}
